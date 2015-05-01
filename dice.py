daaf04a928512354ec075efa53e3f3ae05a6a21f
gabe2o2 <ggomez@kippnyccp.org>
2015-05-01 13:24:38 -0400

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,7 @@
+from random import randint
+number = {2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0}
+
+for i in range(500):
+  rolls = randint(1,6) + randint(1,6)
+  number(rolls) += 1
+print number(rolls) / 500
