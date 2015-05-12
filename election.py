6c140e2469cf3e6fe77acb9c88baca3aada75dcc
gabe2o2 <ggomez@kippnyccp.org>
2015-05-12 13:12:48 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,14 +1,14 @@
 from random import randint
-total = 0
-for i in range(10000):
-  wins = 0 
-  if randint(1,100) <= 87:
+total = 0 #sets the total equal to 0
+for i in range(10000): #repeats the process below 10,000 times since the range in 10000
+  wins = 0 #makes wins equal to 0
+  if randint(1,100) <= 87: #picks a number between 1-100 and compares it to 87 to see if that region is won
     wins = wins+1
-  if randint(1,100) <= 65:
+  if randint(1,100) <= 65: #same as comment on line 5, but with it compared to 65
     wins = wins+1
-  if randint(1,100) <= 17: 
+  if randint(1,100) <= 17: #same as comment on line 7, but with it compared to 17
     wins = wins+1
-  if wins >= 2
+  if wins >= 2: #checks to see if wins is greater than or equal to 2, and then adds 1 to total
     total += 1 
-winning = (total/((float(10000)))
-print "your chance in winning is %s" % chance
+chance = (total/((float(10000))) * 100 #gives probability of winning
+print "your chance in winning is %s%" % chance #statement that is printed out
