3584d8d15749018dbcb1cc610584c698f4af90ed
gabe2o2 <ggomez@kippnyccp.org>
2015-05-12 13:08:52 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,14 +1,14 @@
 from random import randint
-total=0
+total = 0
 for i in range(10000):
-  wins=0 
+  wins = 0 
   if randint(1,100) <= 87:
-    wins=wins+1
+    wins = wins+1
   if randint(1,100) <= 65:
-    wins=wins+1
+    wins = wins+1
   if randint(1,100) <= 17: 
-    wins=wins+1
-  if wins>=2
-    total+=1 
-winning= (total/((float(10000)))
-print "your chance in winning is %s" & chance
+    wins = wins+1
+  if wins >= 2
+    total += 1 
+winning = (total/((float(10000)))
+print "your chance in winning is %s" % chance
